c8f3bf69bd19f2bd0e2a8b7ff5cb2730df6a874d
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-06-10 22:14:33 +0200

    Add test for multiple invocations

--- spec/default_method_return_impersonation_spec.rb
+++ spec/default_method_return_impersonation_spec.rb
@@ -17,6 +17,24 @@ describe 'Default method return impersonation', clear_recordings: true do
     end
   end
 
+  it 'can record and impersonate multiple invocations in a row of a method that returns a simple value' do
+    Impersonator.recording('simple value') do
+      impersonator = Impersonator.impersonate(real_object, :foo)
+      expect(impersonator.foo).to eq(:bar)
+      expect(impersonator.foo).to eq(:bar)
+      expect(real_object).to be_invoked
+    end
+
+    real_object.reset
+
+    Impersonator.recording('simple value') do
+      impersonator = Impersonator.impersonate(real_object, :foo)
+      expect(impersonator.foo).to eq(:bar)
+      expect(impersonator.foo).to eq(:bar)
+      expect(real_object).not_to be_invoked
+    end
+  end
+
   pending 'raises an error when trying to impersonate without starting a recording'
   pending 'raises an error when the method to impersonate does not exist'
 
